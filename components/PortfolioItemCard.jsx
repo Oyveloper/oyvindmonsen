853d5ee2df63c232979fab53293c6c2c250dc984
Øyvind Monsen <yvind.monsen@gmail.com>
2021-02-11 10:38:18 +0100

    displaying url for projects that have one

--- components/PortfolioItemCard.jsx
+++ components/PortfolioItemCard.jsx
@@ -3,6 +3,14 @@ import React from "react";
 import urlFor from "../core/imageBuilder";
 
 const PortfolioItemCard = ({ portfolioItem }) => {
+  const title =
+    portfolioItem.url == "" ? (
+      <h3 className="font-bold text-lg">{portfolioItem.name}</h3>
+    ) : (
+      <a href={portfolioItem.url} className="underline">
+        <h3 className="font-bold text-lg">{portfolioItem.name}</h3>
+      </a>
+    );
   return (
     <div className="group h-80 w-full overflow-hidden rounded-lg shadow-md block relative">
       <img
@@ -12,7 +20,7 @@ const PortfolioItemCard = ({ portfolioItem }) => {
       />
 
       <div className="block max-h-full portfolio-info-section bg-white w-full px-8 py-5 absolute bottom-0">
-        <h3 className="font-bold text-lg">{portfolioItem.name}</h3>
+        {title}
         <div className="transition-max-height duration-500 ease-in-out max-h-0 h-auto overflow-hidden group-hover:max-h-96">
           <BlockContent blocks={portfolioItem.description} />
         </div>
